3b264fc711a193c062390ac0a52a5f4ab1b1dd99
Ashutosh Singh <ashutosh.singh19071999@gmail.com>
2022-04-30 15:05:31 +0530

    Implemented Ask Me Single Permission with help of Interfaces and Made Dialog for showing Dialog Box for the deny Permission for the message entered by the user.

--- AskMe/src/main/java/com/redeyesncode/askme/RedEyesNCode.kt
+++ AskMe/src/main/java/com/redeyesncode/askme/RedEyesNCode.kt
@@ -1,8 +1,80 @@
 package com.redeyesncode.askme
 
-class RedEyesNCode {
+import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
+open class RedEyesNCode(var context: Context,var activity: Activity) {
+
+    //CONCEPT OF THE COMPANION OBJECT TO GET THE ALL THE GLOBAL VARIABLES VALUE OF THE CLASS INTO ANY OTHER CLASS.
+    companion object{
+        var PERMISSION_GRANTED_REQUEST=99;
+        var PERMISSION_REQUEST_CODE =77;
+        var PERMISSION_DENIED_CODE =-100
+        var PERMISSION_REQUESTING_CODE =66;
+    }
+
 
     //DEFINING  THE RUNTIME PERMISSION AND THE OTHER FUNCTIONS AND INTERFACES REALTED TO PERMISSIONS HERE !
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun askMeSinglePermission(permission:String,dialogBoxDenyMessage:String,permissionObservers: permissionObservers){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            //runtime permission are only allowed after android marshmellow.
+            if( context.checkSelfPermission(permission)==PackageManager.PERMISSION_GRANTED){
+                //THE PERMISSION IS GRANTED SUCCESSFULLY YOU CAN USE THE API REQUESTING THE PERMISSION NOW.
+                permissionObservers.onPermissionResultObserver(PERMISSION_GRANTED_REQUEST)
+
+            }else{
+                //NEED TO REQUEST THE PERMISSION FROM THE USER.
+                    //INTIALIZING AN ARRAY IN KOTLIN WITH THE FIXED SIZE
+                    var permissionArray : Array<String> =Array<String>(1){permission}
+                activity.requestPermissions(permissionArray,PERMISSION_REQUEST_CODE)
+
+
+            }
+        }
+
+    }
+
+    interface permissionObservers{
+        fun onPermissionResultObserver(permissionCode: Int)
+
+
+    }
+
+    fun showPermissionDenyDialog(message:String){
+        var dialog = Dialog(context,R.style.RoundedCornersDialog)
+        dialog.setContentView(R.layout.common_dialog_box_warning)
+
+        var  textViewMessage: TextView
+        textViewMessage = dialog.findViewById(R.id.tvSetText)
+        var btnOk:LinearLayout
+        btnOk = dialog.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener {
+            dialog.dismiss()
+        }
+        textViewMessage.text = message
+        dialog.setCancelable(true)
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.show()
+
+
+
+    }
+
+
+
+
+
+
 
 
 }
--- app/src/main/java/com/redeyesncode/askmee/MainActivity.kt
+++ app/src/main/java/com/redeyesncode/askmee/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.redeyesncode.askmee
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.redeyesncode.askme.RedEyesNCode
+import com.redeyesncode.askmee.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,RedEyesNCode.permissionObservers {
+    lateinit var binding : ActivityMainBinding
+    override fun onPermissionResultObserver(permissionCode: Int) {
+
+        //THIS METHOD GET NOTIFIED EVERY-TIME IF THE PERMISSION IS ALREADY GRANTED.
+        if(RedEyesNCode.PERMISSION_GRANTED_REQUEST==permissionCode){
+            Toast.makeText(this@MainActivity,"PERMISSION IS ALREADY GRANTED !",Toast.LENGTH_SHORT).show()
+        }else if (RedEyesNCode.PERMISSION_DENIED_CODE==permissionCode){
+            Toast.makeText(this@MainActivity,"PERMISSION IS DENIED !",Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAskCamera.setOnClickListener {
+            //CODE FOR THE LIBARY GOES HERE !
+            var redEyesNCode = RedEyesNCode(context = this,this)
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                redEyesNCode.askMeSinglePermission(android.Manifest.permission.CAMERA,"Need this permission to take photos.",this)
+            }
+        }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==RedEyesNCode.PERMISSION_REQUEST_CODE && permissions.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this@MainActivity,"PERMISSION IS GRANTED !",Toast.LENGTH_SHORT).show()
+        }else{
+            var redEyesNCode = RedEyesNCode(context = this,this)
+            redEyesNCode.showPermissionDenyDialog("This features requires permission of camera to take photos.")
+        }
+    }
+
 }
